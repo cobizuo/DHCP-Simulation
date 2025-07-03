14b77ca789876fb1c0d8e1c154f377f1449ac07a
CJ Obizuo <66581590+cobizuo@users.noreply.github.com>
2025-07-03 00:55:02 -0400

    Server instantiation logic

--- server/dhcp_server.py
+++ server/dhcp_server.py
@@ -2,6 +2,7 @@ import time
 import random
 import threading
 import socket
+import ipaddress
 
 class DHCPServer:
     
@@ -11,11 +12,25 @@ class DHCPServer:
         self.mask = mask
         self.lease_time = lease_time
 
-        self.free_ips = []
+        self.free_ips = populate_ips(network, mask)
         self.pending_offers = {}
         self.leases = {}
 
         print("DHCP Server initialized for:", network, " mask:", mask)
+    
+
+    def populate_ips(self, network, mask):
+        network_mask = self.mask.split('.')
+        mask_type
+        if network_mask[2] != 0:
+            mask_type = "A"
+            network_ip = str(network_mask[0])
+
+        elif network_mask[1] != 0:
+            mask_type = "B"
+        else:
+            mask_type = "C"
+
     
 
     def run(self):
